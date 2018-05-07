374c2d2047692ca50de0e57b33b627ed156a1046
Caleb Spare <cespare@gmail.com>
2018-05-07 15:26:42 -0700

    Add -utc

--- tsprefix.go
+++ tsprefix.go
@@ -10,11 +10,16 @@ import (
 
 func main() {
 	layout := flag.String("format", time.Stamp, "Time format string (see http://golang.org/pkg/time/)")
+	utc := flag.Bool("utc", false, "Use UTC instead of local time")
 	flag.Parse()
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Printf("%s %s\n", time.Now().Format(*layout), scanner.Bytes())
+		t := time.Now()
+		if *utc {
+			t = t.UTC()
+		}
+		fmt.Printf("%s %s\n", t.Format(*layout), scanner.Bytes())
 	}
 	// Don't bother checking scanner.Err().
 }
